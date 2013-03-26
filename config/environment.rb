ff46ec9ae2b4f9bf8fe93c3356cf00a945705a4e
Phat Wangrungarun <phatograph@gmail.com>
2013-03-26 21:16:58 +0700

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+Phatograph2013::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phatograph2013::Application.config.secret_token = 'a81e9026b2f57b3dfa1130aca530cd05ff1b004da1fac7ad2eeb7c552ff308c2839940aff1427d95450e6589b12c0e8530979ed96e5d6e0bdaa71f7c6fcc691d'
